f85f7d774bf3636471e92caec819d53adcc90da4
jorgeProgramaEn5 <116350790+jorgeProgramaEn5@users.noreply.github.com>
2023-05-30 19:21:12 -0400

    add funtion search

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Hero, ListCard, Texto, Footer } from "./Components";
 import "./styles/App.css"
-import { banner, } from './Constant';
+import { banner, card_data } from './Constant';
 import { useState } from "react";
 // import { socrates_500x450, pinker_500x450, haidt_500x450 } from './Assets';
 // import Tarjeta from "./Components/Tarjeta";
@@ -13,11 +13,14 @@ function App() {
 const [searchValue, setSearchValue] = useState('');
 
   const handleSearchChange = (event) => {
-    console.log(searchValue);
     setSearchValue(event.target.value);
   };
 
   // searchValue={searchValue}
+
+  const targets = card_data.filter(data =>{
+    return data.title.toLowerCase().includes(searchValue.toLowerCase())
+  })
 
   return (
     <div className="app row">
@@ -32,7 +35,7 @@ const [searchValue, setSearchValue] = useState('');
           <div className="col"><Texto /></div>
         </div>
         <div className="row d-flex">
-          <div className="col-md-12"><ListCard /></div>
+          <div className="col-md-12"><ListCard targets={targets} /></div>
         </div>
         <div className="row">
           <div className="col-md-12"><Footer /></div>
--- src/Components/ListCard.jsx
+++ src/Components/ListCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { card_data } from '../Constant';
+// import { card_data } from '../Constant';
 import { Card } from "./index.js";
 import '../styles/ListCard.css'
 
 
 
-const ListCard = () => {
+const ListCard = ({ targets }) => {
   return (
     <div className='row'>
-        {card_data.map((card)=>(
+        {targets.map((card)=>(
           <div className='col-md-3 continer__listcard' key={card.id}> 
             <Card images={ card.img } title={card.title}/>
           </div>
